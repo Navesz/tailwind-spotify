98b574b9acf57f53b552dd7823cb0d3338af628e
Navesz <leonardonaves121@gmail.com>
2023-06-11 16:48:09 -0300

    readme.md modify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ export function Footer() {
           <div className='flex items-center gap-6'>
             <Shuffle size={20} className='text-zinc-200'/>
             <SkipBack size={20} className='text-zinc-200'/>
-            <button className='w-1t0 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
               <Play />
             </button>
             <SkipForward size={20} className='text-zinc-200'/>
